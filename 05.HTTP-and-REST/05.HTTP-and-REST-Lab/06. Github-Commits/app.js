0a93ef0f6a9b0b2ed927f85ee51ce73010f31e63
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-03-25 14:57:02 +0200

    Update app.js

--- 05.HTTP-and-REST/05.HTTP-and-REST-Lab/06. Github-Commits/app.js
+++ 05.HTTP-and-REST/05.HTTP-and-REST-Lab/06. Github-Commits/app.js
@@ -20,24 +20,27 @@ async function loadCommits() {
    }
 }
 
-// function loadCommits() {
-//     const BASE_URL = 'https://api.github.com/repos/';
-//     const username = document.getElementById('username');
-//     const repo = document.getElementById('repo');
-//     const ul = document.getElementById('commits');
+//Solution 02
 
-//     fetch(`${BASE_URL}${username.value}/${repo.value}/commits`)
-//         .then((response) => response.json())
-//         .then((result) => {
-//             result.forEach(result => {
-//                 const newLi = document.createElement('li');
-//                 newLi.textContent = `${result.commit.author.name}: ${result.commit.message}`;
-//                 ul.appendChild(newLi);
-//             })
-//         })
-//         .catch((error)=> {           
-//             const newLi = document.createElement('li');
-//             newLi.textContent = `Error: ${error.message} (Not found)`
-//             ul.appendChild(newLi);
-//         })
-//     }
+function loadCommits() {
+    const BASE_URL = 'https://api.github.com/repos/';
+    const username = document.getElementById('username');
+    const repo = document.getElementById('repo');
+    const ul = document.getElementById('commits');
+
+    fetch(`${BASE_URL}${username.value}/${repo.value}/commits`)
+        .then((response) => response.json())
+        .then((result) => {
+            result.forEach(result => {
+                const newLi = document.createElement('li');
+                newLi.textContent = `${result.commit.author.name}: ${result.commit.message}`;
+                ul.appendChild(newLi);
+            })
+        })
+        .catch((error)=> {           
+            const newLi = document.createElement('li');
+            newLi.textContent = `Error: ${error.message} (Not found)`
+            ul.appendChild(newLi);
+        })
+    }
+
