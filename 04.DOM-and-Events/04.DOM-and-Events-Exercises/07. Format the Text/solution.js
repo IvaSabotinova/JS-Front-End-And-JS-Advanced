9e2711a7ed3189b92121503715e104e681fdcddc
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-04-12 15:02:37 +0300

    Update solution.js

--- 04.DOM-and-Events/04.DOM-and-Events-Exercises/07. Format the Text/solution.js
+++ 04.DOM-and-Events/04.DOM-and-Events-Exercises/07. Format the Text/solution.js
@@ -1,17 +1,45 @@
 function solve() {
-  const textArea = document.getElementById('input');
-  const output = document.getElementById('output');
 
-  const sentences = textArea.value.toString().split('.').filter(x => x !== '');
-
-  while (sentences.length > 0) {
-    let threeParas = sentences.splice(0, 3).map(x => x.trimStart());
-    let threeParasAsString = threeParas.join('. ') + '.';
-    let newHTMLPara = document.createElement('p');
-    newHTMLPara.textContent = threeParasAsString;
-    output.appendChild(newHTMLPara);
-
+  const inputSelectors = {
+    input: document.getElementById('input')
   }
+
+  const otherSelectors = {
+    formatBtn: document.getElementById('formatItBtn'),
+    output: document.getElementById('output'),
+  }
+
+
+  const text = inputSelectors.input.value;
+  if (text) {
+
+    const splittedText = text.split('.');
+    const trimmedText = splittedText.map(x => x.trimStart()).filter(x => x !== '');
+
+    while (trimmedText.length > 0) {
+
+      const currentParaText = trimmedText.splice(0, 3).join('. ');
+      otherSelectors.output.innerHTML += `<p>${currentParaText + '.'}</p>`;
+
+    }
+    inputSelectors.input.value = '';
+  }
+
+//Another solution
+
+  // const textArea = document.getElementById('input');
+  // const output = document.getElementById('output');
+
+  // const sentences = textArea.value.toString().split('.').filter(x => x !== '');
+
+  // while (sentences.length > 0) {
+  //   let threeParas = sentences.splice(0, 3).map(x => x.trimStart());
+  //   let threeParasAsString = threeParas.join('. ') + '.';
+  //   let newHTMLPara = document.createElement('p');
+  //   newHTMLPara.textContent = threeParasAsString;
+  //   output.appendChild(newHTMLPara);
+
+  // }
 
 }
 
